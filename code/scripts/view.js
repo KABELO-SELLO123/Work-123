4618949c18583768fd512e9ee7f965025ae61723
KABELO-SELLO123 <yunggeekralliejs@gmail.com>
2023-04-03 16:09:09 +0200

    first commit

---
+++ code/scripts/view.js
@@ -0,0 +1,27 @@
+window.addEventListener('load',()=>{
+
+    var getCardh=localStorage.getItem('CardTom');
+    var getCardb=localStorage.getItem('CardBottom');
+    var getCardNum=localStorage.getItem('CardNum');
+    var getCardAmount=localStorage.getItem('CardAmount');
+    var CardColorStyle=localStorage.getItem('CardC');
+
+    document.getElementById('claim-option').textContent=getCardb;
+    document.getElementById('pay-group').textContent=getCardb;
+
+    PasscardInfromation(getCardh,getCardb,getCardNum,getCardAmount,CardColorStyle)
+})
+
+
+function PasscardInfromation(getCardh,getCardb,getCardNum,getCardAmount,CardColorStyle){
+
+    document.getElementById('get-card-h').textContent=getCardh;
+    document.getElementById('get-card-b').textContent=getCardb;
+    document.getElementById('get-card-Number').textContent=getCardNum;
+    document.getElementById('get-card-Amount').textContent=getCardAmount;
+    document.getElementById('set-card-color').className=CardColorStyle;
+    
+}
+
+
+
